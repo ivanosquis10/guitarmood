3a19b5823f9708f995ec67dff7f77c9424d5fe15
Ivan Rodriguez <ivanosquis02.08@gmail.com>
2023-09-09 21:30:29 -0400

    Updated: fix responsive design in the cart item, now looks nice

--- components/CartItem/CartItem.tsx
+++ components/CartItem/CartItem.tsx
@@ -9,12 +9,12 @@ const CartItem = ({ guitar }: { guitar: GuitarShop }) => {
   const { deleteItem, addQuantity, minusQuantity } = useCartStore()
 
   return (
-    <div className="flex p-2 gap-x-2 md:gap-x-0 md:p-4 rounded border border-zinc-700/50 shadow-zinc-800">
+    <div className="flex flex-col md:flex-row p-2 gap-2 md:gap-x-0 md:p-4 rounded border border-zinc-700/50 shadow-zinc-800">
       <div className="h-36 w-36 flex-shrink-0 overflow-hidden rounded-md border  bg-white">
         <img
           src={guitar.image}
-          width={220}
-          height={220}
+          width='auto'
+          height='auto'
           alt={`Guitar ${guitar.nombre}`}
           className="h-full w-full object-contain object-center"
         />
@@ -26,7 +26,7 @@ const CartItem = ({ guitar }: { guitar: GuitarShop }) => {
             <h3 className='text-xl md:text-2xl font-bold text-white capitalize'>
               {guitar.nombre}
             </h3>
-            <p className="ml-4">{formatMoney(guitar.precio)}</p>
+            <p className="">{formatMoney(guitar.precio)}</p>
           </div>
           <div className="flex justify-between text-base text-zinc-500">
             <p className='capitalize'>
@@ -35,7 +35,7 @@ const CartItem = ({ guitar }: { guitar: GuitarShop }) => {
             <p className="ml-4 text-sm tracking-wider">{formatMoney(guitar.precio * guitar.quantity)}</p>
           </div>
         </div>
-        <div className="flex flex-1 items-end justify-between text-sm">
+        <div className="flex flex-1 items-end justify-between text-sm mt-5 md:mt-0">
           <div className='flex items-center gap-x-1'>
 
             <p className="text-base md:text-lg">Qty {guitar.quantity}</p>
