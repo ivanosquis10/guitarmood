72e12085aafca890a111932440ceb80bfd61190d
ivanosquis10 <ivanosquis10.12@gmail.com>
2023-09-15 14:50:32 -0500

    try to fix this storage omg

--- store/store.tsx
+++ store/store.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/indent */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 'use client'
@@ -17,30 +18,100 @@ interface Store {
   calculateTotal: () => number
 }
 
+// export const useCartStore = create<Store>((set, get) => ({
+//   cart: localStorage.getItem('cart') != null ? JSON.parse(localStorage.getItem('cart')!) : [],
+//   reviews: { average: 4, totalCount: 117 },
+//   addToCart: (item: GuitarShop) => {
+//     set((state) => {
+//       // validamos si esta en el carrito
+//       const isAlreadyInCart = state.cart.find((guitar) => guitar.id === item.id)
+//       if (isAlreadyInCart != null) {
+//         // en caso de que este, iteramos para conseguirlo
+//         const cartUpdate = state.cart.map((guitar) => {
+//           // comparamos el id
+//           if (guitar.id === item.id) {
+//             // y actualizamos la cantidadme dice
+//             return { ...guitar, quantity: guitar.quantity + item.quantity }
+//           }
+//           return guitar
+//         })
+//         // get().calculateTotal()
+//         return { ...state, cart: cartUpdate }
+//       }
+
+//       // get().calculateTotal()
+//       toast.success('Guitar added successfully', {
+//         duration: 1000,
+//         position: 'top-right'
+//       })
+//       return { ...state, cart: [...state.cart, item] }
+//     })
+//   },
+//   deleteItem: (id: number) => {
+//     set((state) => {
+//       const cartUpdate = state.cart.filter((guitar) => guitar.id !== id)
+
+//       toast.error('Guitar eliminated successfully', {
+//         duration: 1000,
+//         position: 'top-right'
+//       })
+
+//       return { ...state, cart: cartUpdate }
+//     })
+//   },
+
+//   addQuantity: (id: number) => {
+//     set((state) => {
+//       const cartUpdate = state.cart.map((guitar) => {
+//         if (guitar.id === id && guitar.quantity <= 10) {
+//           return { ...guitar, quantity: guitar.quantity + 1 }
+//         }
+//         return guitar
+//       })
+//       return { ...state, cart: cartUpdate }
+//     })
+//   },
+//   minusQuantity: (id: number) => {
+//     set((state) => {
+//       const cartUpdate = state.cart.map((guitar) => {
+//         if (guitar.id === id && guitar.quantity > 1) {
+//           return { ...guitar, quantity: guitar.quantity - 1 }
+//         }
+//         return guitar
+//       })
+//       return { ...state, cart: cartUpdate }
+//     })
+//   },
+//   calculateTotal: () => {
+//     return get().cart.reduce((acc, guitar) => acc + guitar.quantity * guitar.precio, 0)
+//   }
+// }))
+
 export const useCartStore = create<Store>()(
   persist(
     (set, get) => ({
-      cart: (localStorage.getItem('cart') != null) ? JSON.parse(localStorage.getItem('cart')!) : [],
+      // cart: (localStorage.getItem('cart-storage') != null) ? JSON.parse(localStorage.getItem('cart-storage')!) : [],
+      cart: (localStorage.getItem('cart-storage') != null) ? JSON.parse(localStorage.getItem('cart-storage')!)?.state?.cart || [] : [],
       reviews: { average: 4, totalCount: 117 },
       addToCart: (item: GuitarShop) => {
         set((state) => {
-          // validamos si esta en el carrito
+          //  validamos si esta en el carrito
           const isAlreadyInCart = state.cart.find((guitar) => guitar.id === item.id)
           if (isAlreadyInCart != null) {
-            // en caso de que este, iteramos para conseguirlo
+            //  en caso de que este, iteramos para conseguirlo
             const cartUpdate = state.cart.map((guitar) => {
-              // comparamos el id
+              //  comparamos el id
               if (guitar.id === item.id) {
-                // y actualizamos la cantidadme dice
+                //  y actualizamos la cantidadme dice
                 return { ...guitar, quantity: guitar.quantity + item.quantity }
               }
               return guitar
             })
-            // get().calculateTotal()
+            get().calculateTotal()
             return { ...state, cart: cartUpdate }
           }
 
-          // get().calculateTotal()
+          get().calculateTotal()
           toast.success('Guitar added successfully', {
             duration: 1000,
             position: 'top-right'
@@ -87,7 +158,80 @@ export const useCartStore = create<Store>()(
         return get().cart.reduce((acc, guitar) => acc + guitar.quantity * guitar.precio, 0)
       }
     }), {
-    name: 'cart'
+    name: 'cart-storage'
   }
   )
 )
+
+/*
+
+export const useCartStore = create<Store>((set, get) => ({
+  cart: [],
+  reviews: { average: 4, totalCount: 117 },
+  addToCart: (item: GuitarShop) => {
+    set((state) => {
+      // validamos si esta en el carrito
+      const isAlreadyInCart = state.cart.find((guitar) => guitar.id === item.id)
+      if (isAlreadyInCart != null) {
+        // en caso de que este, iteramos para conseguirlo
+        const cartUpdate = state.cart.map((guitar) => {
+          // comparamos el id
+          if (guitar.id === item.id) {
+            // y actualizamos la cantidadme dice
+            return { ...guitar, quantity: guitar.quantity + item.quantity }
+          }
+          return guitar
+        })
+        // get().calculateTotal()
+        return { ...state, cart: cartUpdate }
+      }
+
+      // get().calculateTotal()
+      toast.success('Guitar added successfully', {
+        duration: 1000,
+        position: 'top-right'
+      })
+      return { ...state, cart: [...state.cart, item] }
+    })
+  },
+  deleteItem: (id: number) => {
+    set((state) => {
+      const cartUpdate = state.cart.filter((guitar) => guitar.id !== id)
+
+      toast.error('Guitar eliminated successfully', {
+        duration: 1000,
+        position: 'top-right'
+      })
+
+      return { ...state, cart: cartUpdate }
+    })
+  },
+
+  addQuantity: (id: number) => {
+    set((state) => {
+      const cartUpdate = state.cart.map((guitar) => {
+        if (guitar.id === id && guitar.quantity <= 10) {
+          return { ...guitar, quantity: guitar.quantity + 1 }
+        }
+        return guitar
+      })
+      return { ...state, cart: cartUpdate }
+    })
+  },
+  minusQuantity: (id: number) => {
+    set((state) => {
+      const cartUpdate = state.cart.map((guitar) => {
+        if (guitar.id === id && guitar.quantity > 1) {
+          return { ...guitar, quantity: guitar.quantity - 1 }
+        }
+        return guitar
+      })
+      return { ...state, cart: cartUpdate }
+    })
+  },
+  calculateTotal: () => {
+    return get().cart.reduce((acc, guitar) => acc + guitar.quantity * guitar.precio, 0)
+  }
+}))
+
+*/
